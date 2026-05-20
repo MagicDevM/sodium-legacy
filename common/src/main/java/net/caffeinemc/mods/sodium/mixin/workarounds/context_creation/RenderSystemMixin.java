c7ee2acee0e646ee08d0dbae6f9bdc8a00ae20ad
magicdevm <aaronfernan78@gmail.com>
2026-05-20 13:56:18 +0530

    fix(RenderSystemMixin): removed Unused & Missing ShaderType Class

--- common/src/main/java/net/caffeinemc/mods/sodium/mixin/workarounds/context_creation/RenderSystemMixin.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/mixin/workarounds/context_creation/RenderSystemMixin.java
@@ -4,7 +4,6 @@ import com.mojang.blaze3d.platform.DisplayData;
 import com.mojang.blaze3d.platform.ScreenManager;
 import com.mojang.blaze3d.platform.Window;
 import com.mojang.blaze3d.platform.WindowEventHandler;
-import com.mojang.blaze3d.shaders.ShaderType;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
 import net.caffeinemc.mods.sodium.client.compatibility.checks.ModuleScanner;
