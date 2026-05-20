2b91328050865f426a187e1a79d37f00372f0fb9
magicdevm <aaronfernan78@gmail.com>
2026-05-20 12:36:47 +0530

    fix(EntityRendererMixin): removed EntityRenderState Class

--- common/src/main/java/net/caffeinemc/mods/sodium/mixin/features/render/entity/cull/EntityRendererMixin.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/mixin/features/render/entity/cull/EntityRendererMixin.java
@@ -5,14 +5,13 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.caffeinemc.mods.sodium.client.render.SodiumWorldRenderer;
 import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.client.renderer.entity.EntityRenderer;
-import net.minecraft.client.renderer.entity.state.EntityRenderState;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(EntityRenderer.class)
-public abstract class EntityRendererMixin<T extends Entity, S extends EntityRenderState> {
+public abstract class EntityRendererMixin<T extends Entity> {
     @WrapOperation(method = "shouldRender", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/culling/Frustum;isVisible(Lnet/minecraft/world/phys/AABB;)Z", ordinal = 0))
     private boolean preShouldRender(Frustum instance, AABB aABB, Operation<Boolean> original, T entity) {
         var renderer = SodiumWorldRenderer.instanceNullable();
@@ -21,6 +20,6 @@ public abstract class EntityRendererMixin<T extends Entity, S extends EntityRend
             return original.call(instance, aABB);
         }
 
-        return renderer.isEntityVisible((EntityRenderer<T, S>) (Object) this, entity) && original.call(instance, aABB);
+        return renderer.isEntityVisible(entity) && original.call(instance, aABB);
     }
 }
