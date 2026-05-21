5d08f1d7a1fa55f8e2ac302aef7ef3fbf2d989aa
magicdevm <aaronfernan78@gmail.com>
2026-05-21 12:28:48 +0530

    fix(ClonedChunkSection): more missing symbols

--- common/src/main/java/net/caffeinemc/mods/sodium/client/world/cloned/ClonedChunkSection.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/client/world/cloned/ClonedChunkSection.java
@@ -29,7 +29,7 @@ public class ClonedChunkSection {
     
     private static final DataLayer DEFAULT_SKY_LIGHT_ARRAY = new DataLayer(15);
     private static final DataLayer DEFAULT_BLOCK_LIGHT_ARRAY = new DataLayer(0);
-    private static final PalettedContainer<BlockState> DEFAULT_STATE_CONTAINER = new PalettedContainer<>(Block.STATE_IDS, Blocks.AIR.getDefaultState(), PalettedContainer.PaletteProvider.BLOCK_STATE);
+    private static final PalettedContainer<BlockState> DEFAULT_STATE_CONTAINER = new PalettedContainer<>(Block.BLOCK_STATE_REGISTRY, Blocks.AIR.defaultBlockState(), PalettedContainer.Strategy.SECTION_STATES);
 
     private final SectionPos pos;
 
@@ -94,7 +94,7 @@ public class ClonedChunkSection {
             return DEFAULT_STATE_CONTAINER;
 
         // We use swapUnsafe in the loops to avoid acquiring/releasing the lock on each iteration
-        var container = new PalettedContainer<>(Block.STATE_IDS, Blocks.AIR.getDefaultState(), PalettedContainer.PaletteProvider.BLOCK_STATE);
+        var container = new PalettedContainer<>(Block.BLOCK_STATE_REGISTRY, Blocks.AIR.defaultBlockState(), PalettedContainer.Strategy.SECTION_STATES);
         if (pos.getY() == 3) {
             // Set the blocks at relative Y 12 (world Y 60) to barriers
             BlockState barrier = Blocks.BARRIER.defaultBlockState();
