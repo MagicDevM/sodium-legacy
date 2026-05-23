e6223f77f8ba314f0ee765639be5f00ee1f961d8
magicdevm <aaronfernan78@gmail.com>
2026-05-23 16:14:20 +0530

    fix(DefaultTerrainRenderPasses); use the get methods to get private fields

--- common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/terrain/DefaultTerrainRenderPasses.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/terrain/DefaultTerrainRenderPasses.java
@@ -3,9 +3,9 @@ package net.caffeinemc.mods.sodium.client.render.chunk.terrain;
 import net.minecraft.client.renderer.RenderType;
 
 public class DefaultTerrainRenderPasses {
-    public static final TerrainRenderPass SOLID = new TerrainRenderPass(RenderType.SOLID, false, false);
-    public static final TerrainRenderPass CUTOUT = new TerrainRenderPass(RenderType.CUTOUT, false, true);
-    public static final TerrainRenderPass TRANSLUCENT = new TerrainRenderPass(RenderType.TRANSLUCENT, true, true);
+    public static final TerrainRenderPass SOLID = new TerrainRenderPass(RenderType.solid(), false, false);
+    public static final TerrainRenderPass CUTOUT = new TerrainRenderPass(RenderType.cutout(), false, true);
+    public static final TerrainRenderPass TRANSLUCENT = new TerrainRenderPass(RenderType.translucent(), true, true);
 
 
     public static final TerrainRenderPass[] ALL = new TerrainRenderPass[] { SOLID, CUTOUT, TRANSLUCENT };
