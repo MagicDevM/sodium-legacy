a399831e5223d13d5442d8be733a5b4c78c38d66
magicdevm <aaronfernan78@gmail.com>
2026-05-24 22:29:34 +0530

    fix(AtlasManagerMixin): wrong return type

--- common/src/main/java/net/caffeinemc/mods/sodium/mixin/features/textures/animations/tracking/AtlasManagerMixin.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/mixin/features/textures/animations/tracking/AtlasManagerMixin.java
@@ -1,7 +1,7 @@
 package net.caffeinemc.mods.sodium.mixin.features.textures.animations.tracking;
 
 import net.caffeinemc.mods.sodium.api.texture.SpriteUtil;
-import net.minecraft.client.renderer.texture.AbstractTexture;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class AtlasManagerMixin {
     // This is used to catch the fire sprite when an entity is on fire and there's no fire blocks in the scene.
     @Inject(method = "getTexture", at = @At(value = "RETURN"))
-    private void sodium$catchUsedSprites(ResourceLocation resourceLocation, CallbackInfoReturnable<AbstractTexture> cir) {
+    private void sodium$catchUsedSprites(ResourceLocation resourceLocation, CallbackInfoReturnable<TextureAtlasSprite> cir) {
         if (cir.getReturnValue() != null) {
             SpriteUtil.INSTANCE.markSpriteActive(cir.getReturnValue());
         }
