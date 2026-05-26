3e1f6c7a162b17e9e417f2b79e430d542d7e3eb7
magicdevm <aaronfernan78@gmail.com>
2026-05-26 23:01:31 +0530

    fix: added some missing stuff

--- common/src/main/java/net/caffeinemc/mods/sodium/client/render/vertex/VertexFormatRegistryImpl.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/client/render/vertex/VertexFormatRegistryImpl.java
@@ -3,6 +3,7 @@ package net.caffeinemc.mods.sodium.client.render.vertex;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import it.unimi.dsi.fastutil.objects.Reference2IntMap;
 import it.unimi.dsi.fastutil.objects.Reference2IntOpenHashMap;
+import net.caffeinemc.mods.sodium.client.render.vertex.VertexFormatDescriptionImpl;
 import net.caffeinemc.mods.sodium.api.vertex.format.VertexFormatRegistry;
 import net.caffeinemc.mods.sodium.api.vertex.format.VertexFormatDescription;
 
