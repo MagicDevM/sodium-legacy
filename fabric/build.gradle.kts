5d14b03a5c2516bafc704f330f5f9090f933b4ec
magicdevm <aaronfernan78@gmail.com>
2026-06-07 12:30:40 +0530

    feat(builds): updated fabric loom

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -4,7 +4,7 @@ import net.fabricmc.loom.task.RemapSourcesJarTask
 plugins {
     id("multiloader-platform")
 
-    id("fabric-loom") version ("1.9.2")
+    id("fabric-loom") version ("1.10.5")
 }
 
 base {
